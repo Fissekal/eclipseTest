732f0e49a4318969fae588af06aed2243d61da33
jannik <shyeinua@gmail.com>
2016-09-02 11:33:15 +0200

    dims

---
+++ dummy/GitTest/src/bam/Hej.java
@@ -0,0 +1,23 @@
+package bam;
+
+public class Hej {
+
+	public static void main(String[] args) {
+		hitBjarkeInHisHead(4);
+		hitBjarkeMore(22);
+	}
+
+	private static void hitBjarkeInHisHead(int i) {
+		for(int j = 0; j < i; j++) {
+			System.out.println("BANG!!!");
+		}
+	}
+	
+	private static void hitBjarkeMore(int i) {
+		for(int j = 0; j < i; j++) {
+			System.out.println("BANGBANG!");
+			System.out.println("AND ANOTHER BANG!");
+		}
+	}
+
+}
